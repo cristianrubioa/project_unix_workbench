6b4093cda5966da7a74fe919500972cbbdbdd73e
cristianrubioa <cristian.rubio@ubidots.com>
2022-05-15 16:04:48 -0500

    Unix Workbench: fix syntax

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,6 +2,45 @@
 # File: guessinggame.sh
 
 
+function Input()
+{
+  local input=0
+  while [[ $input -eq 0 ]]
+  do
+    echo ""
+    read -p "Guess number: " guess
+    if ! [[ "$guess" =~ ^[0-9]+$ ]]
+    then
+      echo -e "Please enter a NUMBER. \n"
+    else
+      let input=1
+    fi
+  done
+}
+
+
+function Guess()
+{
+  local found=0
+  while [[ $found -eq 0 ]]
+  do
+    Input
+    if [[ $guess -lt $answer ]]
+    then
+        echo " Your guess is less than the correct answer :("
+    elif [[ $guess -gt $answer ]]
+    then
+        echo " Your guess is greater than the correct answer :("
+    else
+        echo " Congratulations you guessed!!"
+        echo " Thanks for playing :)"
+        let found=1
+    fi  
+  done
+}
+
+
+
 echo "-------------------------------------------------"
 echo "         ->      Guess in game      <-           "
 echo "-------------------------------------------------"
@@ -9,38 +48,5 @@ echo " * Guess number of files in current directory "
 echo " Help: Type a number and then press [Enter]"
 
 
-let correctAnswer=`find . -type f | grep -v git | wc -l`
-let found=0
-
-function getInput()
-{
-  local validInput=0
-  while [[ validInput -eq 0 ]]
-  do
-    read -p 'Guess number: ' guess
-    if [[ $guess =~(^[+,-]{0,1}[0-9]+) ]]
-    then
-      let validInput=1
-    else
-      echo 'Please enter a NUMBER.'
-      echo ""
-   fi
-  done
-}
-
-while [[ $found -ne 1 ]]
-do
-    echo ""
-    getInput 
-    if [[ guess -lt $correctAnswer ]]
-    then
-        echo " Your guess is less than the correct answer :("
-    elif [[ guess -gt $correctAnswer ]]
-    then
-        echo " Your guess is greater than the correct answer :("
-    else
-        echo " Congratulations! you guessed!!"
-        echo " Thanks for playing :)"
-        let found=1
-    fi   
-done
+let answer=`find . -type f | grep -v git | wc -l`
+Guess
